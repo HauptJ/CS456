3bb07528c45171dba0771563cf5bafe33ff7d155
Ryan Owens <ryanvader82@gmail.com>
2017-09-11 11:54:51 -0500

    Finished QuickSort with tests

--- Proj1/src/sorters/QuickSort.py
+++ Proj1/src/sorters/QuickSort.py
@@ -1,1 +1,34 @@
 #!/usr/bin/env python3
+from sorters.Sorter import Sorter
+
+
+class QuickSort(Sorter):
+    """docstring for QuickSort."""
+
+    def __init__(self, A):
+        super(QuickSort, self).__init__(A)
+
+    def sort(self):
+        B = self.A.copy()
+        self.quicksort(B, 0, len(B) - 1)
+        return B
+
+    def quicksort(self, A, p, r):
+        if p < r:
+            q = self.partition(A, p, r)
+            self.quicksort(A, p, q - 1)
+            self.quicksort(A, q + 1, r)
+
+    def partition(self, A, p, r):
+        x = A[r]
+        i = p - 1
+        for j in range(p, r):
+            if A[j] <= x:
+                i = i + 1
+                temp = A[j]
+                A[j] = A[i]
+                A[i] = temp
+        temp = A[i + 1]
+        A[i + 1] = A[r]
+        A[r] = temp
+        return i + 1
--- Proj1/src/sorters/RadixSort.py
+++ Proj1/src/sorters/RadixSort.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+from sorters.Sorter import Sorter
+
+
+class RadixSort(Sorter):
+    """docstring for RadixSort."""
+
+    def __init__(self, A):
+        super(RadixSort, self).__init__(A)
+
+    def sort(self):
+        B = self.A.copy()
+
+        return B
